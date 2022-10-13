15663c808f87b3c279315ede75874f9da7ffb32c
suti1979 <suti1979@gmail.com>
2022-10-13 15:59:27 +0200

    close button

--- client/src/Components/Coctail.tsx
+++ client/src/Components/Coctail.tsx
@@ -3,14 +3,19 @@ import { ICoctailData } from "./App"
 export const Coctail = ({ coctail, setSelected, selected }: ICoctailData | any) => {
   // i still know... set down setter is an ugly solution... fix it later
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    if (selected === event.currentTarget.id) setSelected("")
-    else setSelected(event.currentTarget.id)
+    setSelected(event.currentTarget.id)
+  }
+
+  const handleClose = (event: React.MouseEvent<HTMLElement>) => {
+    event.stopPropagation()
+    setSelected("")
   }
 
   const selectedClass = selected === coctail.name ? "selected" : ""
 
   return (
     <div className={`card ${selectedClass}`} onClick={handleClick} id={coctail.name}>
+      {selectedClass && <div className="btn-close" onClick={handleClose}></div>}
       <img src={coctail.thumbnail} alt="coctail" />
       <div className="coctail">
         <div>{coctail.name}</div>
