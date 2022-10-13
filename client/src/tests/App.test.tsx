3261d2bd6449e9eda20b0b28a9e549c167c8e374
suti1979 <suti1979@gmail.com>
2022-10-13 16:45:00 +0200

    setup basic frontend test

--- client/src/tests/App.test.tsx
+++ client/src/tests/App.test.tsx
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from '../Components/App';
+import { render, screen, cleanup, fireEvent } from "@testing-library/react"
+import App from "../Components/App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome/i);
-  expect(linkElement).toBeInTheDocument();
-});
+afterEach(cleanup)
+
+describe("Main functionality 3000ms wait to load", () => {
+  test("Renders main page, and click event", () => {
+    setTimeout(() => {
+      render(<App />)
+      const linkElement = screen.getByText(/Welcome to/i)
+      expect(linkElement).toBeInTheDocument()
+
+      const coctail_ABC = screen.getByText("ABC")
+      fireEvent.click(coctail_ABC)
+     
+
+    }, 3000)
+  })
+})
