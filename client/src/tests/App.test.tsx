82d2b79b0422e1f4485f9afe4fa3d357b842e0ad
suti1979 <suti1979@gmail.com>
2022-10-13 17:33:00 +0200

    clearup for today

--- client/src/tests/App.test.tsx
+++ client/src/tests/App.test.tsx
@@ -1,19 +1,19 @@
-import { render, screen, cleanup, fireEvent } from "@testing-library/react"
+import { render, cleanup } from "@testing-library/react"
 import App from "../Components/App"
 
 afterEach(cleanup)
 
-describe("Main functionality 3000ms wait to load", () => {
-  test("Renders main page, and click event", () => {
-    setTimeout(() => {
-      render(<App />)
-      const linkElement = screen.getByText(/Welcome to/i)
-      expect(linkElement).toBeInTheDocument()
+describe("Main functionality wait to load", () => {
+  test("Renders main page, and click event", async () => {
+    const { getByText } = render(<App />)
+    // await waitFor(() => {
+    //   expect(getByText("wellcome")).toBeInTheDocument()
+    // })
 
-      const coctail_ABC = screen.getByText("ABC")
-      fireEvent.click(coctail_ABC)
-     
+    // const coctail_ABC = screen.getByText("ABC")
+    // fireEvent.click(coctail_ABC)
 
-    }, 3000)
+    //Todo: mock Api
+    // check selected state
   })
 })
